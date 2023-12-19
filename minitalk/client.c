77d14e825be7d3b6514e15c4711a0c38350918cd
Aimen Taoussi <aitaouss@e3r6p8.1337.ma>
2023-12-19 21:57:12 +0100

    geloo

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: aitaouss <aitaouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 16:44:14 by aitaouss          #+#    #+#             */
-/*   Updated: 2023/12/18 20:51:19 by aitaouss         ###   ########.fr       */
+/*   Updated: 2023/12/19 21:53:25 by aitaouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,57 +17,99 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void	send_signal(int pid, unsigned char character)
-{
-	int				i;
-	unsigned char	temp_char;
+// void	send_signal(int pid, unsigned char character)
+// {
+// int				i;
+// unsigned char temp_char;
+// unsigned char	temp_char;
 
-	i = 8;
-	temp_char = character;
-	while (i > 0)
-	{
-		i--;
-		temp_char = character >> i;
-		if (temp_char % 2 == 0)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		usleep(42);
-	}
-}
-void set_client_pid(pid_t pid)
-{
-    static pid_t client_pid = 0;
-    client_pid = pid;
-}
+// i = 8;
+// temp_char = character;
+// while (i > 0)
+// {
+// 	i--;
+// 	temp_char = character >> i;
+// 	if (temp_char % 2 == 0)
+// 		kill(pid, SIGUSR2);
+// 	else
+// 		kill(pid, SIGUSR1);
+// 	usleep(100);
+// }
 
-int	main(int argc, char **argv)
-{
-	int	i;
-	int	server_pid;
+// i = 7;
+// while (i >= 0)
+// {
 
+// 	if (((character >> i) & 1) == 0)
+// 		kill(pid, SIGUSR1);
+// 	else
+// 		kill(pid, SIGUSR2);
+// 	usleep (100);
+// 	i--;
+// }
+//}
+// void set_client_pid(pid_t pid)
+// {
+//     static pid_t client_pid = 0;
+//     client_pid = pid;
+// }
+void just_banner()
+{
 	ft_printf("    __  ___  ____   _   __   ____   ______   ___       __       __ __\n");
-    ft_printf("   /  |/  / /  _/  / | / /  /  _/  /_  __/  /   |     / /      / //_/\n");
-    ft_printf("  / /|_/ /  / /   /  |/ /   / /     / /    / /| |    / /      / ,<   \n");
-    ft_printf(" / /  / / _/ /   / /|  /  _/ /     / /    / ___ |   / /___   / /| |  \n");
-    ft_printf("/_/  /_/ /___/  /_/ |_/  /___/    /_/    /_/  |_|  /_____/  /_/ |_| \n");
+	ft_printf("   /  |/  / /  _/  / | / /  /  _/  /_  __/  /   |     / /      / //_/\n");
+	ft_printf("  / /|_/ /  / /   /  |/ /   / /     / /    / /| |    / /      / ,<   \n");
+	ft_printf(" / /  / / _/ /   / /|  /  _/ /     / /    / ___ |   / /___   / /| |  \n");
+	ft_printf("/_/  /_/ /___/  /_/ |_/  /___/    /_/    /_/  |_|  /_____/  /_/ |_| \n");
 	printf("\n");
-	i = 0;
+}
+int main(int argc, char **argv)
+{
+	int i;
+	int start;
+	pid_t server_pid;
+	unsigned char character;
+	int	done = 0;
+	just_banner();
 	if (argc != 3)
 	{
-		printf("\033[30m\033[101m ERROR \033[0m \033[97mUsage: %s <server_pid>\n", argv[0]);	
+		printf("\033[30m\033[101m ERROR \033[0m \033[97mUsage: %s <server_pid>\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
-	server_pid = atoi(argv[1]);
-	kill(server_pid(getpid()));
-	set_client_pid(getpid());
+    server_pid = atoi(argv[1]);
+	start = 0;
+	// if (argc == 3)
+
 	if (argv[2][0] == '\0')
-    {
-        printf("Error: Message is empty.\n");
-        return (EXIT_FAILURE);
-    }
-	ft_printf("\033[30m\033[102m DONE \033[0m \033[97mSignal Received.\n");
-	while (argv[2][i])
-		send_signal(server_pid, argv[2][i++]);
+	{
+		printf("Error: Message is empty.\n");
+		return (EXIT_FAILURE);
+	}
+	{
+		while (argv[2][start])
+		{
+			i = 7;
+			character = argv[2][start];
+			while (i >= 0)
+			{
+				if (((character >> i) & 1) == 0)
+					kill(server_pid, SIGUSR1);
+				else
+					kill(server_pid, SIGUSR2);
+				usleep(150);
+				i--;
+			}
+			start++;
+		}
+	}
+	if ((kill(server_pid, SIGUSR1)) != -1)
+	{
+		ft_printf("\033[30m\033[102m DONE \033[0m \033[97mSignal Send.\n");
+		done = 1;
+	}
+	if (server_pid != getpid() && done != 1)
+	{
+		ft_printf("\033[30m\033[101m ERROR \033[0m \033[97mThe Client PID does Not equal to the Server PID");
+		return (EXIT_FAILURE);
+	}
 	return (EXIT_FAILURE);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: aitaouss <aitaouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 17:11:28 by aitaouss          #+#    #+#             */
-/*   Updated: 2023/12/18 20:49:19 by aitaouss         ###   ########.fr       */
+/*   Updated: 2023/12/19 21:53:30 by aitaouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,65 +17,132 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-pid_t	get_c_pid(void)
-{
-	static pid_t client_pid = 0;
-	return client_pid;
-}
+// pid_t g_client_pid = 0;
 
-void	set_c_pid(pid_t pid)
-{
-	static pid_t	client_pid = 0;
-	client_pid = pid;
-}	
-void	signal_handler(int signum)
-{
-	static unsigned char	bit;
-	static int				i;
-	static pid_t			last_c_pid = 0;
+// pid_t	get_c_pid(void)
+// {
+// 	return g_client_pid;
+// }
 
-	if (signum == SIGUSR1)
-	{
-		ft_printf("\033[30m\033[43m RESET \033[0m \033[97mClient PID changed. Resetting...\n");
-		last_c_pid = 0;
-		return ;
-	}
-	bit |= (signum == SIGUSR1);
-	i++;
-	if (i == 8)
-	{
-		ft_printf("%c", bit);
-		bit = 0;
-		i = 0;
-	}
+// void	set_c_pid(pid_t pid)
+// {
+// 	g_client_pid = pid;
+// }	
+// void	signal_handler(int signum, siginfo_t *info, void *context)
+// {
+// 	static unsigned char	bit;
+// 	static int				i;
+// 	static pid_t			last_c_pid = 0;
+	
+// 	(void)info;
+// 	(void)context;
+	
+// 	// if (signum == SIGUSR1)
+// 	// {
+// 	// 	ft_printf("\033[30m\033[43m RESET \033[0m \033[97mClient PID changed. Resetting...\n");
+// 	// 	last_c_pid = 0;
+// 	// 	return ;
+// 	// }
+	
+// 	pid_t					current_c_pid = get_c_pid();
+	
+// 	if (current_c_pid != last_c_pid)
+// 	{
+// 		ft_printf("\033[30m\033[43m RESET \033[0m \033[97mClient PID changed. Resetting...\n");
+// 		last_c_pid = current_c_pid;
+// 		bit = 0;
+// 		i = 0;
+// 	}
+// 	bit |= (signum == SIGUSR2);
+// 	i++;
+// 	if (i == 8)
+// 	{
+// 		write(1, &bit, 1);
+// 		//ft_printf("%c", bit);
+// 		bit = 0;
+// 		i = 0;
+// 	}
+// 	else
+// 		bit <<= 1;
+// }
+
+// int	main(void)
+// {
+// 	struct sigaction	action;
+
+	// ft_printf("    __  ___  ____   _   __   ____   ______   ___       __       __ __\n");
+    // ft_printf("   /  |/  / /  _/  / | / /  /  _/  /_  __/  /   |     / /      / //_/\n");
+    // ft_printf("  / /|_/ /  / /   /  |/ /   / /     / /    / /| |    / /      / ,<   \n");
+    // ft_printf(" / /  / / _/ /   / /|  /  _/ /     / /    / ___ |   / /___   / /| |  \n");
+    // ft_printf("/_/  /_/ /___/  /_/ |_/  /___/    /_/    /_/  |_|  /_____/  /_/ |_| \n");
+	// printf("\n");
+	// ft_printf("\033[30m\033[42m DONE \033[0m \033[97mMinitalk Server PIP : [ %d ]\n", getpid());
+// 	action.sa_sigaction = &signal_handler;
+// 	action.sa_flags = SA_SIGINFO;
+	
+// 	sigaction(SIGUSR2, &action, NULL);
+// 	sigaction(SIGUSR1, &action, NULL);
+// 	while (1)
+// 		pause();
+// 	return (EXIT_SUCCESS);
+// }
+void handle_signal(int signal, siginfo_t *info, void *context)
+{
+    static unsigned char    character = 0;
+    static pid_t            current_pid;
+    static pid_t            client_pid;
+    static int                i = 7;
+
+    (void)context;
+    if (!client_pid)
+        client_pid = info->si_pid;
+    current_pid = info->si_pid;
+    if (current_pid != client_pid)
+    {
+        client_pid = current_pid;
+        character = 0;
+        i = 7;
+    }
+	if (signal == SIGUSR1)
+		character |= (0 << i);
 	else
-		bit <<= 1;
-	pid_t	current_c_pid = get_c_pid();
-	if (current_c_pid != last_c_pid)
+		character |= (1 << i);
+	i--;
+    if (i < 0)
 	{
-		ft_printf("\033[30m\033[43m RESET \033[0m \033[97mClient PID changed. Resetting...\n");
-		last_c_pid = current_c_pid;
-		bit = 0;
-		i = 0;
+		write(1, &character, 1);
+		character = 0;
+		i = 7;
 	}
 }
-
-int	main(void)
+	
+void print_server()
 {
-	struct sigaction	action;
-
 	ft_printf("    __  ___  ____   _   __   ____   ______   ___       __       __ __\n");
     ft_printf("   /  |/  / /  _/  / | / /  /  _/  /_  __/  /   |     / /      / //_/\n");
     ft_printf("  / /|_/ /  / /   /  |/ /   / /     / /    / /| |    / /      / ,<   \n");
     ft_printf(" / /  / / _/ /   / /|  /  _/ /     / /    / ___ |   / /___   / /| |  \n");
     ft_printf("/_/  /_/ /___/  /_/ |_/  /___/    /_/    /_/  |_|  /_____/  /_/ |_| \n");
 	printf("\n");
-	ft_printf("\033[30m\033[42m DONE \033[0m \033[97mMinitalk Server PIP : [ %d ]\n", getpid());
-	action.sa_handler = &signal_handler;
-	action.sa_flags = 0;
-	sigaction(SIGUSR2, &action, NULL);
-	sigaction(SIGUSR1, &action, NULL);
-	while (1)
-		pause();
-	return (EXIT_SUCCESS);
+	ft_printf("\033[30m\033[42m WAITING \033[0m \033[97mMinitalk Server PIP : [ %d ]\n", getpid());
+}
+
+int main(void)
+{
+    struct sigaction    action;
+
+    action.sa_sigaction = &handle_signal;
+    action.sa_flags = SA_SIGINFO;
+
+    print_server();
+    
+
+    while (1)
+    {
+        sigaction(SIGUSR2, &action, NULL);
+        sigaction(SIGUSR1, &action, NULL);
+        pause();
+
+    }
+    return (0);
 }
