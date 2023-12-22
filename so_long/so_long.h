6876fca0cece1c3443a6ff9f90b2428b6df60a29
Aimen Taoussi <aitaouss@e3r6p8.1337.ma>
2023-12-22 21:48:35 +0100

    so_long

---
+++ so_long/so_long.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aitaouss <aitaouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 19:36:08 by aitaouss          #+#    #+#             */
+/*   Updated: 2023/12/22 20:15:30 by aitaouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOLONG_H
+# define SOLONG_H
+
+# include <mlx.h>
+# include <stdlib.h>
+# include <math.h>
+
+# define RED "\033[30m\033[101m"
+# define GREEN "\033[30m\033[42m"
+# define WHITE "\033[0m \033[97m"
+# define BLUE "\033[0m\033[94m"
+# define LIGHT_BLUE "\033[0m\033[96m"
+# define YELLOW "\033[0m\033[92m"
+# define GREEN_NEW "\033[92m"
+# define RED_NEW "\033[31m"
+
+#endif
